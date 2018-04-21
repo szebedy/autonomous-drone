d30dcdcd23f6864564829de73c97ad17adda722b
bszebedy <bszebedy@student.ethz.ch>
2018-04-21 15:58:03 +0200

    Cleanup offboard navigation code

--- src/ex1/takeoff_n_land.cpp
+++ src/ex1/takeoff_n_land.cpp
@@ -11,6 +11,8 @@
 #include <mavros_msgs/CommandTOL.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <mavros_msgs/PositionTarget.h>
+#include <tf/tf.h>
 
 #define FLIGHT_ALTITUDE 1.5f
 
@@ -29,18 +31,15 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
 
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, state_cb);
-    ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::PoseArray>
-            ("whycon/poses", 10, pose_cb);
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-            ("mavros/setpoint_position/local", 10);
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
-            ("mavros/cmd/arming");
-    ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandTOL>
-      ("mavros/cmd/land");
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
-            ("mavros/set_mode");
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+    ros::Subscriber pose_sub = nh.subscribe<geometry_msgs::PoseArray>("whycon/poses", 10, pose_cb);
+
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+    ros::Publisher setpoint_pub = nh.advertise<mavros_msgs::PositionTarget>("mavros/setpoint_raw/local", 10);
+
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/land");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
@@ -56,6 +55,9 @@ int main(int argc, char **argv)
     pose.pose.position.x = 0;
     pose.pose.position.y = 0;
     pose.pose.position.z = FLIGHT_ALTITUDE;
+    pose.pose.orientation = tf::createQuaternionMsgFromYaw(0.5f);
+
+    mavros_msgs::PositionTarget setpoint;
 
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
@@ -80,19 +82,15 @@ int main(int argc, char **argv)
 
     // change to offboard mode and arm
     while(ros::ok() && !current_state.armed){
-        if( current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0))){
-          ROS_INFO(current_state.mode.c_str());
-            if( set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent){
+        if( current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(5.0))){
+            ROS_INFO(current_state.mode.c_str());
+            if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent){
                 ROS_INFO("Offboard enabled");
             }
             last_request = ros::Time::now();
         } else {
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success){
+            if( !current_state.armed && (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) && arm_cmd.response.success){
                     ROS_INFO("Vehicle armed");
                 }
                 last_request = ros::Time::now();
@@ -110,15 +108,19 @@ int main(int argc, char **argv)
 
     ROS_INFO("Taking off");
     for(int i = 0; ros::ok() && i < 10*20; ++i){
-      local_pos_pub.publish(pose);
-      ros::spinOnce();
-      rate.sleep();
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
     }
     ROS_INFO("Takeoff finished! Looking for whycon marker");
 
-    for(int j = 0; ros::ok() && j < 10; ++j){
+    /*for(int j = 0; ros::ok() && j < 10; ++j){
       if (ros::Time::now() - current_pose.header.stamp < ros::Duration(5.0)) {
         ROS_INFO("Marker found, approaching");
+        if (current_pose.poses[0].position.z < 2) {
+          ROS_INFO("Changing orientation");
+          pose.pose.orientation = current_pose.poses[0].orientation;
+        }
         if (current_pose.poses[0].position.z < 1) {
           if (current_pose.poses[0].position.z < 0.6) {
             ROS_INFO("Close enough");
@@ -133,7 +135,7 @@ int main(int argc, char **argv)
         pose.pose.position.z -= current_pose.poses[0].position.y;
         //send setpoint for 5 seconds
         for(int i = 0; ros::ok() && i < 5*20; ++i){
-          local_pos_pub.publish(pose);
+          setpoint_pub.publish(pose);
           ros::spinOnce();
           rate.sleep();
         }
@@ -146,53 +148,23 @@ int main(int argc, char **argv)
 
     ROS_INFO("Marker approached!");
 
-    /*// go to the third waypoint
-    pose.pose.position.x = 1;
-    pose.pose.position.y = 1;
-    pose.pose.position.z = FLIGHT_ALTITUDE;
-    //send setpoints for 10 seconds
-    ROS_INFO("going to third way point");
-    for(int i = 0; ros::ok() && i < 10*20; ++i){
-
-      local_pos_pub.publish(pose);
-      ros::spinOnce();
-      rate.sleep();
-    }
-    ROS_INFO("third way point finished!");
-    
-    // go to the forth waypoint
-    pose.pose.position.x = 1;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = FLIGHT_ALTITUDE;
-    //send setpoints for 10 seconds
-    ROS_INFO("going to forth way point");
-    for(int i = 0; ros::ok() && i < 10*20; ++i){
-
-      local_pos_pub.publish(pose);
-      ros::spinOnce();
-      rate.sleep();
-    }
-    ROS_INFO("forth way point finished!");
-    
+    // Back to the origin
     pose.pose.position.x = 0;
     pose.pose.position.y = 0;
     pose.pose.position.z = FLIGHT_ALTITUDE;
-    ROS_INFO("going back to the first point!");
-    //send setpoints for 10 seconds
-    for(int i = 0; ros::ok() && i < 10*20; ++i){
 
-      local_pos_pub.publish(pose);
+    for(int i = 0; ros::ok() && i < 10*20; ++i){
+      setpoint_pub.publish(pose);
       ros::spinOnce();
       rate.sleep();
     }*/
 
     ROS_INFO("Tring to land");
-    while (!(land_client.call(land_cmd) &&
-            land_cmd.response.success)){
-      //local_pos_pub.publish(pose);
-      ROS_INFO("Tring to land");
-      ros::spinOnce();
-      rate.sleep();
+    while (!(land_client.call(land_cmd) && land_cmd.response.success)){
+        //local_pos_pub.publish(pose);
+        ROS_INFO("Tring to land");
+        ros::spinOnce();
+        rate.sleep();
     }
     return 0;
 }
