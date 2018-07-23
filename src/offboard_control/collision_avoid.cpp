e3defd690faf7bb4c8648713dedeb4a62a79940a
bszebedy <bszebedy@student.ethz.ch>
2018-07-23 15:24:16 +0200

    Added new class named CollisionAvoid for collision avoidance 2

---
+++ src/offboard_control/collision_avoid.cpp
@@ -0,0 +1,11 @@
+#include "include/collision_avoid.h"
+
+CollisionAvoid::CollisionAvoid()
+{
+}
+
+
+geometry_msgs::PoseStamped CollisionAvoid::avoid(geometry_msgs::PoseStamped setpoint_pos_ENU, const sensor_msgs::Image &depth_cam_img)
+{
+
+}
